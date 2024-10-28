7a67ae466f120380ace32ba8d9ab84a94dc6dd79
Mariano Capiliku (Angola) <mariano.capiliku@mea.co.ao>
2024-10-28 07:55:35 +0000

    applying cros orgin proxy, to allow use the suggetion api without any extension

--- src/utils/conts.ts
+++ src/utils/conts.ts
@@ -2,4 +2,4 @@ export const MY_YOUTUBE_KEY = "AIzaSyACiM3hzVh_b35-dhP-2DBMDiF20qlGgsU";
 
 export const YOUTUBE_API = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=US&key=${MY_YOUTUBE_KEY}`;
 
-export const YOUTUBE_SEARCH_SUGGESTIONS_API = `http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=`;
+export const YOUTUBE_SEARCH_SUGGESTIONS_API = `//corsproxy.io/?http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=`;
