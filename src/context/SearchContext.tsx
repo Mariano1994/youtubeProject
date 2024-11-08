bdff7503ed3873ac3f26c8005629a43d000cd6b8
Mariano Capiliku <marianocapiliku@gmail.com>
2024-11-08 23:20:35 +0100

    Created cache for search inputs and its results. It will prevent unecessary api calls

--- src/context/SearchContext.tsx
+++ src/context/SearchContext.tsx
@@ -8,18 +8,22 @@ interface ChildrenProps {
 interface ContextProvider {
   searchQuery: string;
   suggestions: string[];
+
   onHandleQuery: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
 export const SearchContext = createContext<ContextProvider>({
   searchQuery: "",
   suggestions: [],
+
   onHandleQuery: () => {},
 });
 
 export const SearchContextProvider = ({ children }: ChildrenProps) => {
   const [searchQuery, setSearchQuery] = useState("");
   const [suggestions, setSuggestions] = useState([]);
+  //Cache to store all the search and its results. Needed to prevent unecessary api calls
+  const [cacheResuts, setCacheResults] = useState<any>({});
 
   useEffect(() => {
     async function getSuggestionsData() {
@@ -28,10 +32,23 @@ export const SearchContextProvider = ({ children }: ChildrenProps) => {
       );
       const suggestionsInfo = await suggestionData.json();
       setSuggestions(suggestionsInfo[1]);
+      //Updating the cache, storing the requested query and its result
+      setCacheResults(() => {
+        return {
+          ...cacheResuts,
+          [searchQuery]: suggestionsInfo[1],
+        };
+      });
     }
 
     const timer = setTimeout(() => {
-      getSuggestionsData();
+      // before make api call, check on cache if requested query is not already on cache. If it's stored on cache don't make api call, instead of that fill the suggestions with the data on cache
+      if (cacheResuts[searchQuery]) {
+        setSuggestions(cacheResuts[searchQuery]);
+      } else {
+        // If the requested query is not on cache, make api call
+        getSuggestionsData();
+      }
     }, 200);
 
     return () => {
@@ -40,7 +57,8 @@ export const SearchContextProvider = ({ children }: ChildrenProps) => {
   }, [searchQuery]);
 
   const onHandleQuery = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setSearchQuery(event.target.value);
+    const query = event.target.value;
+    setSearchQuery(query);
   };
 
   return (
